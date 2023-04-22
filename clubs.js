8df00654699ee8edb6102b2d1522bf3245bcbb1c
AlexMaunouryBenavides <alexandremaunoury@yahoo.fr>
2023-04-22 19:23:44 +0200

    first commit

---
+++ clubs.js
@@ -0,0 +1,293 @@
+export const clubs = [
+  {
+    name: 'PSG',
+    ville: 'Paris',
+    stade: 'Parc des Princes',
+    capacity: "49 691",
+    surface: "Pelouse naturel",
+    image:"https://lh3.googleusercontent.com/p/AF1QipNlF3ZAxh0AbfqGaAEHZKCuqbmDmVUbCEBoOdPa=s680-w680-h510"
+  },
+  {
+    name: 'Olympique de Marseille',
+    stade: 'Orange Vélodrome',
+    capacity: "67 394",
+    surface: "Pelouse naturel",
+    image:"https://www.orangevelodrome.com/wp-content/uploads/2020/08/Conformation-photos-histoire-6-1366x768-1.jpg"
+  },
+  {
+    name: 'Ajaccio',
+    stade: 'Stade François Coty',
+    capacity: "10 446",
+    surface: "Pelouse naturel",
+    image:"https://scontent.fbod1-1.fna.fbcdn.net/v/t1.6435-9/123326989_10157403054582301_5722968511045231137_n.jpg?stp=dst-jpg_p180x540&_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=YT-5WoVQwAIAX84PuXE&_nc_ht=scontent.fbod1-1.fna&oh=00_AfCB5taTxEsSZZx2Q1SNwawuNbgpzL1OokaLIIa50bBn7Q&oe=6460B7AC"
+  },
+  {
+    name: 'Angers',
+    stade: 'Satde Raymond-Kopa',
+    capacity: "19 350",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/6/6e/Stade-RK-AngersSCO.jpg"
+  },
+  {
+    name: 'Monaco',
+    stade: 'Satde Louis II',
+    capacity: "18 523",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Panoramio_-_V%26A_Dudush_-_stade_Louis_II.jpg/640px-Panoramio_-_V%26A_Dudush_-_stade_Louis_II.jpg"
+  },
+  {
+    name: 'Brest',
+    stade: 'Stade Francis le Blé',
+    capacity: "15 097",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/0/0c/Stade_Francis-Le_Bl%C3%A9.JPG"
+  },
+  {
+    name: 'Troyes',
+    stade: "Stade de l'Aube",
+    capacity: "21 877",
+    surface: "Pelouse naturel",
+    image:"https://www.troyeslachampagne.com/wp-content/uploads/wpetourisme/686955_stade_de_laube_visite_ambassadeurs_sm_-_troyes_champagne_tourisme_21.jpg"
+  },
+  {
+    name: 'Lorient',
+    stade: 'Stade du Moustoir',
+    capacity: "19 010",
+    surface: "Pelouse sunthétique",
+    image:"https://upload.wikimedia.org/wikipedia/commons/b/b1/Stade_du_Moustoir.jpg"
+  },
+  {
+    name: 'Montpellier',
+    stade: 'Stade de la Mosson',
+    capacity: "32 939",
+    surface: "Pelouse naturel",
+    image:"https://www.info-stades.fr/uploads/stades/montpellier-stade-de-la-mosson7.jpg"
+  },
+  {
+    name: 'Nantes',
+    stade: 'Stade de la Beaujoire- Louis Fonteneau',
+    capacity: "37 463",
+    surface: "Pelouse naturelle",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Stade_de_la_Beaujoire%2C_vue_de_la_tribune_pr%C3%A9sidentielle.jpg/800px-Stade_de_la_Beaujoire%2C_vue_de_la_tribune_pr%C3%A9sidentielle.jpg"
+  },
+  {
+    name: 'Nice',
+    stade: 'Allianz Riviera',
+    capacity: "36 178",
+    surface: "Pelouse naturelle",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Allianzcoupdenvoi.jpg/1280px-Allianzcoupdenvoi.jpg"
+  },
+  {
+    name: 'Rennes',
+    stade: 'Roazhon Park',
+    capacity: "29 778",
+    surface: "Pelouse naturelle",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Roazhon_Park_-_France_Paraguay_June_2nd_2017.jpg/1200px-Roazhon_Park_-_France_Paraguay_June_2nd_2017.jpg"
+  },
+  {
+    name: 'Toulouse',
+    stade: 'Stadium Municipal',
+    capacity: "33 150",
+    surface: "Pelouse naturelle",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Stadium-Lory.jpg/1200px-Stadium-Lory.jpg"
+  },
+  {
+    name: 'Strasbourg',
+    stade: 'Stade de la Meinau',
+    capacity: "29 000",
+    surface: "Pelouse naturel",
+    image:"https://www.tchapp.alsace/assets/image-cache/ARTICLES/2019/Mai/stade_de_la_meinau.603a40d1.jpg"
+  },
+  {
+    name: 'Reims',
+    stade: 'Stade Auguste-Delaune II',
+    capacity: "21 684",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/9/96/Tribune_Batteux.JPG"
+  },
+  {
+    name: 'Olympique Lyonnais',
+    ville: 'Lyon',
+    stade: 'Groupama Stadium',
+    capacity: "59 186",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/d/d2/Int%C3%A9rieur_POL.JPG"
+  },
+  {
+    name: 'RC Lens',
+    stade: 'Stade Bollaert-Delelis',
+    capacity: "38 223",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Stade_Bollaert_Delelis.JPG/800px-Stade_Bollaert_Delelis.JPG"
+  },
+  {
+    name: 'Clermont-Ferrand',
+    stade: 'Stade Gabriel Montpied',
+    capacity: "13 000",
+    surface: "Pelouse naturel",
+    image:"https://img.lamontagne.fr/5YPxWBWVr_xm_EyHM5csmfhUrfyBaBx8_4mKeOZ39rc/fit/657/438/sm/0/bG9jYWw6Ly8vMDAvMDAvMDYvMTkvNzgvMjAwMDAwNjE5NzgzMQ.jpg"
+  },
+  {
+    name: 'Auxerre',
+    stade: "Stade de l'Abbé Deschamps",
+    capacity: "18 541",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/0/0e/Stade_Abb%C3%A9-Deschamps_%289%29.jpg"
+  },
+  {
+    name: 'Lille',
+    stade: 'Décathlon Arena-Stade Pierre-Mauroye',
+    capacity: "50 186",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Russia_v_Slovakia_%282016-06-15%29_18.jpg/1920px-Russia_v_Slovakia_%282016-06-15%29_18.jpg"
+  },
+      // Club anglais
+  {
+    name: 'Manchester United',
+    stade: 'Old Trafford',
+    capacity: "74 879",
+    surface: "Pelouse hybride",
+    image:"https://www.si.com/.image/t_share/MTkxMTk2ODg3MjA5MzU0NDc1/imago1011329909h.jpg"
+  },
+  {
+    name: 'West Ham United',
+    ville: 'Londre',
+    stade: 'London Stadium',
+    capacity: "66 000",
+    surface: "Pelouse hybride",
+    image:"https://gtfc.co.uk/wp-content/uploads/2022/05/London-Stadium.jpg"
+  },
+  {
+    name: 'Tottenham',
+    ville: 'Londre',
+    stade: 'Tottenham Hotspur Stadium',
+    capacity: "62 850",
+    surface: "Pelouse naturel",
+    image:"https://populous.com/wp-content/uploads/2018/01/Populous_Tottenham-Hotspur-Stadium_Credit-Edward-Hill_3.jpg"
+  },
+  {
+    name: 'Arsenal',
+    ville: 'Londre',
+    stade: 'Emirates Stadium',
+    capacity: "60 704",
+    surface: "Pelouse naturel",
+    image:"https://www.arsenal.com/sites/default/files/styles/large_16x9/public/images/emirates-fisheye-general.png?itok=xBCOgR2S"
+  },
+  {
+    name: 'Manchester City',
+    stade: 'Etihad Stadium',
+    capacity: "55 017",
+    surface: "Pelouse hybride",
+    image:"https://upload.wikimedia.org/wikipedia/commons/b/b8/Etihad_Stadium.jpg"
+  },
+  {
+    name: 'Liverpool',
+    stade: 'Anfield',
+    capacity: "54 074",
+    surface: "Pelouse hybride",
+    image:"https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg):focal(1340x680:1342x678)/origin-imgresizer.eurosport.com/2021/02/16/2995806-61471848-2560-1440.jpg"
+  },
+  {
+    name: 'Newcastle',
+    stade: "St James'Park",
+    capacity: "52 338",
+    surface: "Pelouse naturel",
+    image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/6a/e9/0f/img-20190209-123322-01.jpg?w=1200&h=-1&s=1"
+  },
+  {
+    name: 'Birmingham',
+    stade: 'Villa Park',
+    capacity: "42 682",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Villa_Park.jpg/800px-Villa_Park.jpg"
+  },
+  {
+    name: 'Chelsea',
+    ville: 'Londre',
+    stade: 'Stamford Bridge',
+    capacity: "40 853",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Stamford_Bridge_Clear_Skies.JPG/1200px-Stamford_Bridge_Clear_Skies.JPG"
+  },
+  {
+    name: 'Everton',
+    ville: 'Liverpool',
+    stade: 'Goodison Park',
+    capacity: "39 571",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/8/88/Goodison_Park%2C_October_2021.jpg"
+  },
+  {
+    name: 'Leeds',
+    stade: 'Elland Road',
+    capacity: "37 890",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Leeds_United_-_31559864360.jpg/1200px-Leeds_United_-_31559864360.jpg"
+  },
+  {
+    name: 'Southampton',
+    stade: "St Mary's Stadium",
+    capacity: "32 384",
+    surface: "Pelouse naturel",
+    image:"https://upload.wikimedia.org/wikipedia/commons/5/52/St_Mary%27s_Stadium_%282%29.jpg"
+  },
+  {
+    name: 'Leicester',
+    stade: 'King Power Stadium',
+    capacity: "32 273",
+    surface: "Pelouse hybride",
+    image:"https://upload.wikimedia.org/wikipedia/commons/b/bb/King_Power_Stadium_wide_view.jpg"
+  },
+  {
+    name: 'Wolverhampton',
+    stade: 'Molineux Stadium',
+    capacity: "32 050",
+    surface: "Pelouse hybride",
+    image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/21/3f/1f/the-molineux-stadium.jpg?w=1200&h=-1&s=1"
+  },
+  {
+    name: 'Brighton & Hove Albion',
+    stade: 'Amex Stadium',
+    capacity: "31 800",
+    surface: "Pelouse naturel",
+    image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/7f/49/b7/20190512-124844-largejpg.jpg?w=1200&h=-1&s=1"
+  },
+  {
+    name: 'Notthingham Forest',
+    stade: 'The city Ground',
+    capacity: "30 445",
+    surface: "Pelouse naturel",
+    image:"https://www.avillafan.com/wp-content/uploads/City-Ground.jpg"
+  },
+  {
+    name: 'Crystal Palace',
+    stade: 'Selhurst Park',
+    capacity: "26 047",
+    surface: "Pelouse hybride",
+    image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/38/d8/17/lrm-export-20170121-231306.jpg?w=1200&h=-1&s=1"
+  },
+  {
+    name: 'Fulham',
+    ville: 'Londre',
+    stade: 'Craven Cottage',
+    capacity: "22 384",
+    surface: "Pelouse naturel",
+    image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/8b/ee/58/craven-cottage.jpg?w=1200&h=-1&s=1"
+  },
+  {
+    name: 'Brentford',
+    stade: 'Brentford Community Stadium',
+    capacity: "17 250",
+    surface: "Pelouse hybride",
+    image:"https://res.cloudinary.com/brentford-fc/image/upload/f_auto,q_auto:best,f_auto,c_fill,ar_16:9/Production/w-1237_h-auto_m-fit_s-any__Pitch_Screen_5_ExileNationLogo_fdyva2.jpg"
+  },
+  {
+    name: 'Bournemouth',
+    stade: 'Vitality Stadium',
+    capacity: "11 329",
+    surface: "Pelouse naturel",
+    image:"https://cdn.resfu.com/img_data/estadios/original_new/167.jpg?size=720x&lossy=1"
+  },
+ 
+]
+
